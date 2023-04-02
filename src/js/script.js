327d585372c345a524fc94ee6dbe641d68fe1ec1
DmitryHash <podolnitsky@gmail.com>
2023-04-02 20:18:30 +0300

    add cart (still need to refine)

--- src/js/script.js
+++ src/js/script.js
@@ -81,3 +81,130 @@ paginationItems.forEach((item, index) => {
     startInterval();
   });
 });
+
+
+
+//пока код к корзине поместил в гланый файл скрипта-----------------------------------------------------------
+
+const cartBtn = document.querySelector(".cart-btn");
+const cartModal = document.querySelector(".cart-modal");
+const closeCartBtn = document.querySelector(".close-cart-btn");
+const clearCartBtn = document.querySelector(".clear-cart-btn");
+const cartTotal = document.querySelector(".cart-total");
+const cartItems = document.querySelector(".cart-items");
+
+let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+function addToCartBusket(name, price) {
+    localStorage.setItem('cart', JSON.stringify(cart));
+    let alreadyInCart = false;
+    for (let i = 0; i < cart.length; i++) {
+        if (cart[i].name === name) {
+            cart[i].quantity++;
+            alreadyInCart = true;
+            break;
+        }
+    }
+    if (!alreadyInCart) {
+        cart.push({ name: name, price: price, quantity: 1 });
+    }
+    showCart();
+}
+
+// cartItems.addEventListener('wheel', function(e) {
+//   e.stopPropagation();
+// });
+
+cartModal.addEventListener('wheel', function(e) {
+  modalContent.scrollTop += e.deltaY;
+  e.preventDefault();
+});
+
+function removeFromCart(name) {
+    localStorage.setItem('cart', JSON.stringify(cart));
+    for (let i = 0; i < cart.length; i++) {
+        if (cart[i].name === name) {
+            cart[i].quantity--;
+            if (cart[i].quantity === 0) {
+                cart.splice(i, 1);
+            }
+            break;
+        }
+    }
+    showCart();
+}
+
+function clearCart() {
+    cart = [];
+    localStorage.setItem('cart', JSON.stringify(cart));
+    showCart();
+}
+
+function showCart() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+    cartItems.innerHTML = "";
+    let totalPrice = 0;
+    for (let i = 0; i < cart.length; i++) {
+        let item = cart[i];
+        let itemPrice = item.price * item.quantity;
+        let cartItem = document.createElement("div");
+        cartItem.classList.add("cart-item");
+        cartItem.innerHTML = `<img src="/src/assets/pictures/catalog/bottle1.jpg" alt="bottle" class="hendrick"> <div class="items-in-busket"> <div class="cart-item-name">${item.name}</div> <div class="cart-item-quantity">x ${item.quantity}</div> <div class="cart-item-price">${itemPrice} $</div> <div class="cart-item-remove" data-name="${item.name}">&times;</div> </div>`;
+        cartItems.appendChild(cartItem);
+        totalPrice += itemPrice;
+    }
+    cartTotal.textContent = totalPrice + " $";
+}
+
+cartBtn.addEventListener("click", () => {
+    cartModal.style.display = "block";
+    showCart();
+});
+
+closeCartBtn.addEventListener("click", () => {
+    cartModal.style.display = "none";
+});
+
+clearCartBtn.addEventListener("click", () => {
+    clearCart();
+});
+
+cartItems.addEventListener("click", (event) => {
+    if (event.target.classList.contains("cart-item-remove")) {
+        let name = event.target.dataset.name;
+        removeFromCart(name);
+    }
+});
+
+let addToCartBtns = document.querySelectorAll(".add-to-cart");
+addToCartBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        let name = btn.dataset.name;
+        let price = btn.dataset.price;
+        addToCartBusket(name, price);
+    });
+});
+
+// // модалка, открывающая оригинал картинки
+// function showFullImageModal(imageUrl) {
+//     const modal = document.createElement('div');
+//     modal.classList.add('modal');
+
+//     const modalContent = document.createElement('div');
+//     modalContent.classList.add('modal-content');
+
+//     const img = document.createElement('img');
+//     img.src = imageUrl;
+//     modalContent.appendChild(img);
+
+//     modal.appendChild(modalContent);
+//     document.body.appendChild(modal);
+
+//     // закрыть модалку на кнопку Закрыть
+//     modal.addEventListener('click', () => {
+//         modal.remove();
+//     });
+// }
+
+
+//----------------------------------------------------------------------------------------------------------------------

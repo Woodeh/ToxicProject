25deee1e0394c070c0e01d0315d2a66599acc542
Arkadiy <woodythps@gmail.com>
2023-04-07 16:32:08 +0300

    huge-refactoring

---
+++ src/js/modules/basket-module/basket.js
@@ -0,0 +1,143 @@
+
+const body = document.body;
+const cartOverlay = document.createElement("div");
+const cartBtn = document.querySelector(".cart-btn");
+const cartModal = document.querySelector(".cart-modal");
+const closeCartBtn = document.querySelector(".close-cart-btn");
+const clearCartBtn = document.querySelector(".clear-cart-btn");
+const cartTotal = document.querySelector(".cart-total");
+const cartItems = document.querySelector(".cart-items");
+
+function disableScroll() {
+  body.classList.add("disable-scroll");
+  cartOverlay.classList.add("cart-overlay");
+  body.appendChild(cartOverlay);
+}
+
+function enableScroll() {
+  body.classList.remove("disable-scroll");
+  cartOverlay.classList.remove("cart-overlay");
+}
+
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+function addToCartBasket(name, price, image) {
+  localStorage.setItem("cart", JSON.stringify(cart));
+  let alreadyInCart = false;
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].name === name) {
+      cart[i].quantity++;
+      alreadyInCart = true;
+      break;
+    }
+  }
+  if (!alreadyInCart) {
+    cart.push({
+      name: name,
+      price: price,
+      quantity: 1,
+      image: image,
+    });
+  }
+  showCart();
+}
+
+function removeFromCart(name) {
+  localStorage.setItem("cart", JSON.stringify(cart));
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].name === name) {
+      cart.splice(i, 1);
+      break;
+    }
+  }
+  showCart();
+}
+
+
+function clearCart() {
+  cart = [];
+  showCart();
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+function showCart() {
+    cartItems.innerHTML = "";
+    let totalPrice = 0;
+    for (let i = 0; i < cart.length; i++) {
+      let item = cart[i];
+      let cartItemImage = document.createElement("img");
+      cartItemImage.src = item.image;
+      let itemPrice = item.price * item.quantity;
+      let cartItem = document.createElement("div");
+      cartItem.classList.add("cart-item");
+      cartItem.innerHTML = `<img src="${item.image}" alt="bottle" class="basket-image">
+          <div class="items-in-busket"> 
+          <div class="cart-item-name">${item.name}</div>
+          <div class="cart-item-quantity"><input type="number" min="1" value="${item.quantity}" data-name="${item.name}" data-price="${item.price}"></div>
+          <div class="cart-item-price">${itemPrice.toFixed(2)} $</div> 
+          </div>
+          <div class="cart-item-remove" data-name="${item.name}">&times;</div>`;
+      cartItems.appendChild(cartItem);
+      // cartItem.appendChild(cartItemImage);
+      totalPrice += itemPrice;
+    }
+  cartTotal.textContent =" $" +  totalPrice.toFixed(2);
+
+  let quantityInputs = document.querySelectorAll(
+    ".cart-item-quantity input[type='number']"
+  );
+  quantityInputs.forEach((input) => {
+    input.addEventListener("change", () => {
+      let name = input.dataset.name;
+      let price = parseFloat(input.dataset.price);
+      let quantity = parseInt(input.value);
+      for (let i = 0; i < cart.length; i++) {
+        if (cart[i].name === name) {
+          cart[i].quantity = quantity;
+          break;
+        }
+      }
+      showCart();
+    });
+  });
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+cartBtn.addEventListener("click", () => {
+  cartModal.style.display = "block";
+  showCart();
+  disableScroll();
+});
+
+closeCartBtn.addEventListener("click", () => {
+  cartModal.style.display = "none";
+  enableScroll();
+});
+
+clearCartBtn.addEventListener("click", () => {
+  clearCart();
+});
+
+cartItems.addEventListener("click", (event) => {
+  if (event.target.classList.contains("cart-item-remove")) {
+    let name = event.target.dataset.name;
+    removeFromCart(name);
+  }
+});
+
+let addToCartBtns = document.querySelectorAll(".add-to-cart");
+addToCartBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    let name = btn.dataset.name;
+    let price = btn.dataset.price;
+    let image = btn.dataset.image;
+    addToCartBasket(name, price, image);
+  });
+});
+
+cartOverlay.addEventListener("click", (event) => {
+  if (event.target === cartOverlay) {
+    cartModal.style.display = "none";
+    enableScroll();
+  }
+});

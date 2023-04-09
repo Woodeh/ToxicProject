aa10f58adf1e9025daac6203834129f69d2f337c
Melamora <a299042495@gmail.com>
2023-04-09 17:16:56 +0200

    made changes to be able to merge

--- src/js/script.js
+++ src/js/script.js
@@ -105,15 +105,3 @@ paginationItems.forEach((item, index) => {
   });
 });
 
-
-//каунтер на big-card-product
-const number = document.querySelector('.number');
-const minus = document.querySelector('.product-description-counter-minus');
-const plus = document.querySelector('.product-description-counter-plus');
-minus.addEventListener('click', () => {
-  number.innerHTML--;
-});
-plus.addEventListener('click', () => {
-  number.innerHTML++;
-});
-
